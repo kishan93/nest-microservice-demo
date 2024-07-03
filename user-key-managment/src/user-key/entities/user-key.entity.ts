ecc87e4d2353df2bff7c68a7f1144043da66ac12
Kishan Nath <kishansnath93@gmail.com>
2024-07-03 21:49:39 +0530

    added NODE_ENV=test to set datetime field for sqlite

--- user-key-managment/src/user-key/entities/user-key.entity.ts
+++ user-key-managment/src/user-key/entities/user-key.entity.ts
@@ -14,6 +14,6 @@ export class UserKey {
   @Column()
   rateLimit: number;
 
-  @Column({ type: 'timestamptz' })
+  @Column({ type: process.env.NODE_ENV == 'test' ? 'datetime' : 'timestamptz' })
   expiration: Date;
 }
